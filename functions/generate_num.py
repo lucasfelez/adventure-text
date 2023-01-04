0f9a9c9f421ddf3e302102607e1822b2c716986b
Lucas felez <69124930+lucasfelez@users.noreply.github.com>
2023-01-04 18:32:11 +0000

    .

--- functions/generate_num.py
+++ functions/generate_num.py
@@ -7,17 +7,14 @@ def generate(how_many):
   estimated_time = round(0.02555 * how_many,2)
   
   nums = []
-  # Generate a list of numbers from 0 to how_many (how_many is an integer)
   print("the estimared time is: " + str(estimated_time) + "seconds")
   time.sleep(3)
   start_time = time.time()
   for i in range(how_many):
     nums.append(random.randint(0,how_many*50))
-    print("left to generate is:",how_many-len(nums)) # print the number of numbers left to generate
-    time.sleep(0.02555) # to save CPU
+    print("left to generate is:",how_many-len(nums))
+    time.sleep(0.02555)
     
   print("it took " + str(round(time.time()-start_time, 2)) + " seconds to generate " + str(how_many) + " numbers")
   return nums
 
-  
-  
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import functions
 from functions import generate_num
 from functions import order_num
 
+#generate a list of 5 random numbers
 numsf = generate_num.generate(5)
 print(numsf)
+
+#order the list in ascending order
 numsford = order_num.order_num(numsf,False)
 print(numsford)
+
